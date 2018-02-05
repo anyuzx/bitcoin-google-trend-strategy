4e8832eafd3c58b0218556628ce68852a4b7710c
anyuzx <stefanshi1988@gmail.com>
2018-02-05 12:32:04 -0600

    fix bug/add version 2/add figure

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-## TRADE BITCOIN USING SIMPLE GOOGLE TREND STRATEGY
+# TRADE BITCOIN USING SIMPLE GOOGLE TREND STRATEGY
 
+## Explanation
 Please read this paper first: https://www.nature.com/articles/srep01684?message-global=remove&utm_source=buffer&utm_medium=twitter&utm_campaign=Buffer:%252BWardPlunet%252Bon%252Btwitter&buffer_share=23ec0&error=cookies_not_supported
 
-This strategy can be summarized as the following:
+This strategy (version 1) can be summarized as the following:
 
 * When the relative change of google trend data for `bitcoin` is positive: BUY
 * When the relative change of google trend data for `bitcoin` is negative: SELL
 
 The only parameters is the `delta_t`, which is used to compute the relative change of google trends data.
+
+The script also includes a improved version which can be summarized as the following:
+
+* When the relative change **ratio**, r, is positive: BUY using f(r) percentage of the available cash
+* When the relative change **ratio**, r, is negive: SELL f(r) percentage of the available BTC
+
+f(r) is a shifted sigmoid function with parameter `scale`.
+
+## Result
+[Imgur](https://i.imgur.com/Y7sUq96.png)
--- bitcoin_google_trend_strategy.py
+++ bitcoin_google_trend_strategy.py
@@ -27,7 +27,7 @@ from pytrends.request import TrendReq
 
 google_trend_data = []
 trends = TrendReq(hl = 'en-US', tz = 360)
-trends.build_payload(['bitcoin'], cat = 0, timeframe = '2017-06-01 2018-01-09', gprop = '')
+trends.build_payload(['bitcoin'], cat = 0, timeframe = '2017-06-01 2018-02-05', gprop = '')
 google_trend_data.append(trends.interest_over_time())
 trends.build_payload(['bitcoin'], cat = 0, timeframe = '2017-01-01 2017-06-01', gprop = '')
 google_trend_data.append(trends.interest_over_time())
@@ -56,6 +56,24 @@ google_trend_data.append(trends.interest_over_time())
 trends.build_payload(['bitcoin'], cat = 0, timeframe = '2011-01-01 2011-06-01', gprop = '')
 google_trend_data.append(trends.interest_over_time())
 
+
+# Normalize the Google Trends Data
+n = len(google_trend_data)
+renorm_factor = 1.0
+for index, item in enumerate(google_trend_data[::-1]):
+    if index > 0.0:
+        first_entry = item['bitcoin'].values[0]
+        renorm_factor *= float(last_entry)/float(first_entry)
+        renorm_array = item['bitcoin'].values * renorm_factor
+        trend_array.extend(list(renorm_array[1:]))
+    else:
+        trend_array = list(item['bitcoin'].values)
+    last_entry = item['bitcoin'].values[-1]
+
+trend_array = np.array(trend_array)
+trend_array = 100.0 * trend_array/trend_array.max()
+
+
 # ---------------------------------------------------------------------------------------------
 
 # download the bitcoin historical data from https://www.coindesk.com/price/
@@ -65,10 +83,10 @@ google_trend_data.append(trends.interest_over_time())
 # 这里是从coindesk网站下载的csv文件
 # 当然也可以用coinmarketcap提供的API
 
-btc_price = pd.read_csv('coindesk-bpi-USD-close_data-2010-07-17_2018-01-09.csv')
+btc_price = pd.read_csv('coindesk-bpi-USD-close_data-2010-07-17_2018-02-05.csv')
 # we only need the price data from 2011-01-01 to 2018-01-09
 # 因为google trend的数据是从2011-01-01开始的, 所以我们只需要一部分的价格数据
-btc_price = btc_price[167:-4] 
+btc_price = btc_price[167:-2] 
 btc_price = btc_price['Close Price'].values
 
 # ---------------------------------------------------------------------------------------------
@@ -80,20 +98,31 @@ btc_price = btc_price['Close Price'].values
 def compute_relative_change(trend_array, delta_t):
     total_time_points = len(trend_array)
     relative_change_array = []
+    relative_change_ratio_array = []
     for i in range(delta_t, total_time_points):
         previous_mean = np.mean(trend_array[i - delta_t:i])
-        relative_change = trend_array[i] - previous_mean
-        relative_change_ratio = relative_change / trend_array[i]
-        relative_change_array.append(relative_change)
+        relative_change = trend_array[i] - previous_mean # compute absolute value of relative change
+        relative_change_ratio = relative_change / previous_mean # compute relative change ratio
+        relative_change_array.append(relative_change) 
+        relative_change_ratio_array.append(relative_change_ratio)
 
     relative_change_array = np.array(relative_change_array).flatten()
-    return relative_change_array
+    relative_change_ratio_array = np.array(relative_change_ratio_array).flatten()
+    return relative_change_array, relative_change_ratio_array
 
 # Function to trade
 # 定义交易函数
 
-def trade(price_array, trend_array, delta_t):
-    relative_change = compute_relative_change(trend_array, delta_t)
+# define function to compute percentage of buy and sell based on
+# the relative change ratio
+# scale is the controlled parameter
+def sigmoid(x, scale):
+    return 2.0/(1.0 + np.exp(-x/scale)) - 1.0
+
+# version 1
+# buy and sell according to the sign of relative change
+def trade_v1(price_array, trend_array, delta_t):
+    relative_change, _ = compute_relative_change(trend_array, delta_t)
     
     cash0 = 1.0
     cash = cash0
@@ -102,6 +131,7 @@ def trade(price_array, trend_array, delta_t):
     asset = cash + num_btc * price
     ASSET = []
     PRICE = []
+    ACTION = []
     for index, change in enumerate(relative_change):
         if index == 0:
             price0 = price_array[delta_t + index]
@@ -110,11 +140,15 @@ def trade(price_array, trend_array, delta_t):
 
         if change < 0.0:
             # sell
+            ACTION.append('sell')
+
             if num_btc > 0.0:
                 cash += num_btc * price
                 num_btc = 0.0
         else:
             # buy
+            ACTION.append('buy')
+
             if cash > 0.0:
                 num_btc += cash / price
                 cash = 0.0
@@ -125,20 +159,65 @@ def trade(price_array, trend_array, delta_t):
 
     ASSET = np.array(ASSET)/cash0
     PRICE = np.array(PRICE)/price0
-    return PRICE, ASSET
+    return PRICE, ASSET, ACTION
+
+# version 2
+# buy and sell according to the ratio of relative change
+def trade_v2(price_array, trend_array, delta_t, scale):
+    _, relative_change_ratio = compute_relative_change(trend_array, delta_t)
+    
+    cash0 = 1.0
+    cash = cash0
+    num_btc = 0.0
+    price = 0.0
+    asset = cash + num_btc * price
+    ASSET = []
+    PRICE = []
+    ACTION = []
+    for index, change in enumerate(relative_change_ratio):
+        if index == 0:
+            price0 = price_array[delta_t + index]
+            
+        price = price_array[delta_t + index]
+
+        if change < 0.0:
+            # sell
+            ACTION.append('sell')
+
+            sell_ratio = np.abs(sigmoid(change, scale = scale))
+            if num_btc > 0.0:
+                cash += sell_ratio * num_btc * price
+                num_btc = num_btc * (1.0 - sell_ratio)
+        else:
+            # buy
+            ACTION.append('buy')
+
+            buy_ratio = np.abs(sigmoid(change, scale = scale))
+            if cash > 0.0:
+                num_btc += cash * buy_ratio / price
+                cash = cash * (1.0 - buy_ratio)
+
+        asset = cash + num_btc * price
+        ASSET.append(asset)
+        PRICE.append(price)
+
+    ASSET = np.array(ASSET)/cash0
+    PRICE = np.array(PRICE)/price0
+    return PRICE, ASSET, ACTION
 
 
 # TEST AND PLOT
 # 测试,画图
 
-# the only parameters is the delta_t
-# delta_t = 33 is the optimal
-# 这个策略唯一的可调参数是delta_t, 我发现delta_t=33是最佳参数
-hold, strategy = trade(btc_price, trend_array, delta_t = 33)
+# for version 1, the only parameters is the delta_t
+# delta_t = 50 is the optimal
+hold, strategy1, _ = trade_v1(btc_price, trend_array, delta_t = 50)
+_, strategy2, _ = trade_v2(btc_price, trend_array, delta_t = 50, scale = 0.2)
 
 fig, ax = plt.subplots()
 ax.plot(np.arange(len(hold)), hold, label='Buy and Hold')
-ax.plot(np.arange(len(strategy)), strategy, label='Google Trends Strategy')
+ax.plot(np.arange(len(strategy1)), strategy1, label='Google Trends Strategy v1')
+ax.plot(np.arange(len(strategy2)), strategy2, label='Google Trends Strategy v2')
 ax.set_xlabel('days')
 ax.set_ylabel('Return')
 ax.set_yscale('log')
